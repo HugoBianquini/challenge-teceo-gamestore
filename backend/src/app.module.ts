a1a83e68ef94a592116fb33ad3b58fd313ba1b85
HugoBianquini <hugobianqui@hotmail.com>
2024-11-06 23:39:09 -0300

    feat(backend): create category module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    CategoryModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CategoryService } from './category.service';
 
-@Controller('category')
-export class CategoryController {}
+@Controller('categories')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  findAll() {
+    return 'All Categories';
+  }
+}
